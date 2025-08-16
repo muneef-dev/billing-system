84a3f7771d4a97e70649bb37a67f6405d173aaba
Muneef Marzook <muneef.dev@gmail.com>
2025-08-16 10:06:15 +0530

    feat: db msql

---
+++ src/main/java/com/example/billingsystem/db/DbConnection.java
@@ -0,0 +1,23 @@
+package com.example.billingsystem.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static DbConnection dbConnection;
+    private final Connection connection;
+
+    public DbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/billing_system","root","1234");
+    }
+
+    public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbConnection==null) ? dbConnection = new DbConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
